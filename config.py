462af90afa765fafc5bf901a140600303949c1ea
khasanovvvv <khasanovv.010@gmail.com>
2026-08-04 13:18:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,10 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "123456789:AAExampleTokenHereReplaceMe")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))
 
+# 1xpanel (SMM/nakrutka xizmatlari) API - Render'ning Environment Variables
+# bo'limidan kiritiladi, GitHub'ga hech qachon yozilmaydi.
+SMM_API_KEY = os.getenv("SMM_API_KEY", "")
+SMM_API_URL = os.getenv("SMM_API_URL", "https://1xpanel.com/api/v2")
+
 # Ma'lumotlar bazasi fayli nomi (o'zgartirish shart emas)
 DB_NAME = "shop.db"
